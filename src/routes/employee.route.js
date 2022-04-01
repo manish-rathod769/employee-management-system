0ddf6d92c56bec2ccbb3caeffbb86f71a7bf6c18
krushit-dudhat <krushitdudhat2001@gmail.com>
2022-04-01 16:58:11 +0530

    add: Employee api end points define

---
+++ src/routes/employee.route.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.post('/employees', );
+router.get('/employees', );
+router.get('/employees/:employeeId', );
+router.put('/employees/:employeeId', );
+router.delete('/employees/:employeeId', );
+router.get('/employees/search', );
+
+router.post('/employees/login' );
+router.patch('/employees/:employeeId', );
