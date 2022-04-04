ec69fad19d190237a5fbddf3f01b727eae83e15d
Manish Rathod <manish@manish.rathod>
2022-04-04 09:42:29 +0530

    add: controller added for soft deleting of client's data

--- src/routes/client.route.js
+++ src/routes/client.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { clientRegisterValidation, clientUpdateDataValidation } from '../controllers/client/client.validator';
-import { clientRegisterController, getAllClientController, clientUpdateDataController, } from '../controllers/client/client.controller';
+import { clientRegisterController, getAllClientController, clientUpdateDataController, clientSoftDeleteController } from '../controllers/client/client.controller';
 
 const route = express.Router();
 
 route.get('/clients', getAllClientController);
 route.post('/clients', clientRegisterValidation, clientRegisterController);
 route.put('/clients/:clientId', clientUpdateDataValidation, clientUpdateDataController);
+route.delete('/clients/:clientId', clientSoftDeleteController);
 
 module.exports = route;
