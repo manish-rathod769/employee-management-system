17f7ac029cf9a09230f693e539079145bc220638
krushit-dudhat <krushitdudhat2001@gmail.com>
2022-04-05 16:17:37 +0530

    add: get single employee data

--- src/routes/employee.route.js
+++ src/routes/employee.route.js
@@ -9,7 +9,7 @@ router.get('/add-employee', employeeController.renderAddEmployeeView);
 
 router.post('/employees', employeeValidator.employeeValidate, employeeController.addEmployee);
 router.get('/employees', employeeController.getEmployee);
-router.get('/employees/:employeeId', );
+router.get('/employees/:employeeId', employeeController.getEmployeeOne);
 router.put('/employees/:employeeId', );
 router.delete('/employees/:employeeId', );
 router.get('/employees/search', );
@@ -17,4 +17,6 @@ router.get('/employees/search', );
 router.post('/employees/login' );
 router.patch('/employees/:employeeId', );
 
+// protected, check for employee login and empoyee side route
+router.get('/profile', employeeController.renderEmployeeProfile);
 module.exports = router;
