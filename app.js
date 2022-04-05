1f4f6be42afb323e0f7fec59e988d04583aca51f
Manish Rathod <manish@manish.rathod>
2022-04-05 11:13:33 +0530

    add: css, fonts, img, js, plugin files added from templete

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 
 import bodyParser from 'body-parser';
 import cors from 'cors';
@@ -10,6 +11,7 @@ import session from 'express-session';
 import employeeRoutes from './src/routes/employee.route';
 import publicRoutes from './src/routes/public';
 import clientRoutes from './src/routes/client.route';
+import adminRoutes from './src/routes/admin.routes';
 
 dotenv.config();
 require('./src/config/sequelize');
@@ -40,5 +42,6 @@ app.set('view engine', 'ejs');
 app.use('/', employeeRoutes);
 // requiring routes
 app.use("/", clientRoutes);
+app.use("/", adminRoutes);
 
 module.exports = app;
