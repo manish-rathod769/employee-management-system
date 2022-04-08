5fbd469963bec4254ab361324ad101935eadf7a6
Manish Rathod <manish@manish.rathod>
2022-04-08 17:58:28 +0530

    fix: Eslint error resolved in client module

--- public/js/client.js
+++ public/js/client.js
@@ -1,160 +1,155 @@
-let disableClient = () => {
-  const clientId = $("#disable-client").val();
-  if (confirm('Are you sure ?') == true) {
-    $.ajax({
-      url: `/admin/clients/${clientId}`,
-      method: 'DELETE',
-      success: () => {
-        alert("Client disabled Successfully...");
-        viewClientWithPagination();
-      },
-      error: (resData) => {
-        alert(resData.responseJSON.errorMessage);
-        viewClientWithPagination();
-      }
-    });
-  }
-}
+/* eslint-disable no-alert */
+/* eslint-disable no-restricted-globals */
+/* eslint-disable no-unused-vars */
+const hideShowField = (fieldsToBeHide, fieldsToBeShown) => {
+  fieldsToBeHide.forEach((field) => {
+    $(field).css('display', 'none');
+  });
+  fieldsToBeShown.forEach((field) => {
+    $(field).css('display', 'block');
+  });
+};
 
-let clientDetails = clientId => {
-  hideShowField(["#clients-add-div", "#add-client", "#pagination", "#clients-data-body"], ["#clients-view-div", "#all-client"]);
-  $("#action").text("View Client");
+const clientDetails = (clientId) => {
+  hideShowField(['#clients-add-div', '#add-client', '#pagination', '#clients-data-body'], ['#clients-view-div', '#all-client']);
+  $('#action').text('View Client');
   $.ajax({
     url: `/admin/clients/${clientId}`,
     method: 'GET',
-    success: resData => {
-      $("#client-edit-name").val(resData.data.name);
-      $("#client-edit-city").val(resData.data.city);
-      $("#client-edit-state").val(resData.data.state);
-      $("#client-edit-country").val(resData.data.country);
-      $("#client-edit-organization").val(resData.data.organization);
-      $("#client-edit-submit").val(resData.data.id);
-      $("#disable-client").val(resData.data.id);
+    success: (resData) => {
+      $('#client-edit-name').val(resData.data.name);
+      $('#client-edit-city').val(resData.data.city);
+      $('#client-edit-state').val(resData.data.state);
+      $('#client-edit-country').val(resData.data.country);
+      $('#client-edit-organization').val(resData.data.organization);
+      $('#client-edit-submit').val(resData.data.id);
+      $('#disable-client').val(resData.data.id);
     },
-    error: resData => {
+    error: (resData) => {
       alert(resData.responseJSON.errorMessage);
-      hideShowField(["#all-client", "#clients-add-div", "#clients-view-div"], ["#add-client", "#pagination", "#clients-data-body"]);
+      hideShowField(['#all-client', '#clients-add-div', '#clients-view-div'], ['#add-client', '#pagination', '#clients-data-body']);
+    },
+  });
+};
 
-    }
-  })
-}
-
-let fetchClientData = (index) => {
-  const recordCount = $("#select-record-count").val();
-  const sortBy = $("#sort-by").val();
-  const sortOrder = $("#sort-order").val();
-  const searchWord = $("#search-by").val();
+const fetchClientData = (index) => {
+  const recordCount = $('#select-record-count').val();
+  const sortBy = $('#sort-by').val();
+  const sortOrder = $('#sort-order').val();
+  const searchWord = $('#search-by').val();
 
   $.ajax({
     url: `/admin/client?page=${index}&count=${recordCount}&sortBy=${sortBy}&sortOrder=${sortOrder}&searchWord=${searchWord}`,
     method: 'GET',
     error: (resData) => {
       alert(resData.responseJSON.errorMessage);
-      $("#search-by").val("");
+      $('#search-by').val('');
     },
     success: (resData) => {
-      $("#clients-data").html("");
+      $('#clients-data').html('');
       if (resData.success) {
-
         const paginationDetails = resData.data.pop();
-        $("#action").text("Clients");
-        $("#all-client").css("display", "none");
+        $('#action').text('Clients');
+        $('#all-client').css('display', 'none');
 
-        hideShowField(["#all-client", "#clients-add-div", "#clients-view-div"], ["#add-client", "#pagination", "#clients-data-body"]);
+        hideShowField(['#all-client', '#clients-add-div', '#clients-view-div'], ['#add-client', '#pagination', '#clients-data-body']);
 
-        $("#clients-count").text(resData.data.length + paginationDetails.before + paginationDetails.after);
-        (!paginationDetails.before) ? $("#previous").addClass("disabled") : $("#previous").removeClass("disabled");
-        (!paginationDetails.after) ? $("#next").addClass("disabled") : $("#next").removeClass("disabled");
+        $('#clients-count').text(resData.data.length + paginationDetails.before + paginationDetails.after);
+        // eslint-disable-next-line no-unused-expressions
+        (!paginationDetails.before) ? $('#previous').addClass('disabled') : $('#previous').removeClass('disabled');
+        // eslint-disable-next-line no-unused-expressions
+        (!paginationDetails.after) ? $('#next').addClass('disabled') : $('#next').removeClass('disabled');
 
-        $("#previous").attr('data-index', Number(index) - 1);
-        $("#next").attr('data-index', Number(index) + 1);
-        $("#current").attr('data-index', Number(index));
+        $('#previous').attr('data-index', Number(index) - 1);
+        $('#next').attr('data-index', Number(index) + 1);
+        $('#current').attr('data-index', Number(index));
 
-        $("#current").text(index);
+        $('#current').text(index);
         if (resData.data.length) {
-          resData.data.forEach(client => {
-            $("#clients-data").append(`
-            <div class="col-md-6 col-lg-6 col-xl-4">
-              <div class="card">
-                <div class="card-body">
-                  <div class="pro-widget-content">
-                    <div class="profile-info-widget">
-                      <div class="profile-det-info">
-                        <p>Name: <span class="text-primary">${client.name}</span></p>
-                        <p>EmailId: <span class="text-primary">${client.email}</span></p>
-                        <p>SlackId: <span class="text-primary">${client.slackId}</span></p>
-                        <p>Organization: <span class="text-primary">${client.organization}</span></p>
+          resData.data.forEach((client) => {
+            $('#clients-data').append(`
+            <div class='col-md-6 col-lg-6 col-xl-4'>
+              <div class='card'>
+                <div class='card-body'>
+                  <div class='pro-widget-content'>
+                    <div class='profile-info-widget'>
+                      <div class='profile-det-info'>
+                        <p>Name: <span class='text-primary'>${client.name}</span></p>
+                        <p>EmailId: <span class='text-primary'>${client.email}</span></p>
+                        <p>SlackId: <span class='text-primary'>${client.slackId}</span></p>
+                        <p>Organization: <span class='text-primary'>${client.organization}</span></p>
                       </div>
                     </div>
                   </div>
-                  <p onclick="clientDetails('${client.id}')" class="float-right text-danger cursor-pointer">view</p>
+                  <p onclick='clientDetails("${client.id}")' class='float-right text-danger cursor-pointer'>view</p>
                 </div>
               </div>
             </div>
           `);
           });
         } else {
-          alert("No more data found !!!");
+          alert('No more data found !!!');
         }
       } else {
         alert(resData.errorMessage);
       }
-    }
+    },
   });
-}
+};
 
-let showThisMuchRecord = () => {
-  viewClientWithPagination();
-}
-
-let sortByField = () => {
-  viewClientWithPagination();
-}
-
-let hideShowField = (fieldsToBeHide, fieldsToBeShown) => {
-  fieldsToBeHide.forEach(field => {
-    $(field).css("display", "none");
-  });
-  fieldsToBeShown.forEach(field => {
-    $(field).css("display", "block");
-  });
-}
-
-let viewClientWithPagination = () => {
-  const index = $("#current").attr("data-index");
+const viewClientWithPagination = () => {
+  const index = $('#current').attr('data-index');
   fetchClientData(index);
-}
+};
 
-$("#previous").on("click", () => {
-  const index = $("#previous").attr("data-index");
-  fetchClientData(index);
-});
+const disableClient = () => {
+  const clientId = $('#disable-client').val();
+  if (confirm('Are you sure ?') === true) {
+    $.ajax({
+      url: `/admin/clients/${clientId}`,
+      method: 'DELETE',
+      success: () => {
+        alert('Client disabled Successfully...');
+        viewClientWithPagination();
+      },
+      error: (resData) => {
+        alert(resData.responseJSON.errorMessage);
+        viewClientWithPagination();
+      },
+    });
+  }
+};
 
-$("#next").on("click", () => {
-  const index = $("#next").attr("data-index");
+$('#previous').on('click', () => {
+  const index = $('#previous').attr('data-index');
   fetchClientData(index);
 });
 
-$("#add-client").on("click", () => {
-  hideShowField(["#clients-data-body", "#pagination", "#add-client", "#clients-view-div"], ["#all-client", "#clients-add-div"]);
-  $("#action").text("Add Client");
+$('#next').on('click', () => {
+  const index = $('#next').attr('data-index');
+  fetchClientData(index);
 });
 
-$("#all-client").on("click", () => {
-  hideShowField(["#all-client", "#clients-add-div", "#clients-view-div"], ["#clients-data-body", "#pagination", "#add-client"]);
+$('#add-client').on('click', () => {
+  hideShowField(['#clients-data-body', '#pagination', '#add-client', '#clients-view-div'], ['#all-client', '#clients-add-div']);
+  $('#action').text('Add Client');
+});
+
+$('#all-client').on('click', () => {
+  hideShowField(['#all-client', '#clients-add-div', '#clients-view-div'], ['#clients-data-body', '#pagination', '#add-client']);
   viewClientWithPagination();
 });
 
-$("#search-by").on('input paste', () => {
+$('#search-by').on('input paste', () => {
   viewClientWithPagination();
 });
 
-$("#client-register-form").on("submit", (event) => {
+$('#client-register-form').on('submit', (event) => {
   event.preventDefault();
 });
 
-if (("#client-register-form").length) {
-  $("#client-register-form").validate({
+if (('#client-register-form').length) {
+  $('#client-register-form').validate({
     rules: {
       name: {
         required: true,
@@ -177,36 +172,36 @@ if (("#client-register-form").length) {
       },
       organization: {
         required: true,
-      }
+      },
     },
     messages: {
       name: {
-        required: "Name is required !!!",
+        required: 'Name is required !!!',
       },
       email: {
-        required: "EmailId is required !!!",
-        email: "Please enter a valid emailId !!!",
+        required: 'EmailId is required !!!',
+        email: 'Please enter a valid emailId !!!',
       },
       slackId: {
-        required: "slackId is required !!!",
+        required: 'slackId is required !!!',
       },
       city: {
-        required: "City is required !!!",
+        required: 'City is required !!!',
       },
       state: {
-        required: "State is required !!!",
+        required: 'State is required !!!',
       },
       country: {
-        required: "Country is required !!!",
+        required: 'Country is required !!!',
       },
       organization: {
-        required: "Organization is required !!!",
-      }
+        required: 'Organization is required !!!',
+      },
     },
-    submitHandler: (from) => {
-      const clientData = $("#client-register-form").serializeArray();
+    submitHandler: () => {
+      const clientData = $('#client-register-form').serializeArray();
       const clientDataObj = {};
-      clientData.forEach(obj => {
+      clientData.forEach((obj) => {
         clientDataObj[obj.name] = obj.value;
       });
 
@@ -220,18 +215,18 @@ if (("#client-register-form").length) {
         },
         error: (resData) => {
           alert(resData.responseJSON.errorMessage);
-        }
+        },
       });
-    }
+    },
   });
 }
 
-$("#client-edit-form").on("submit", (event) => {
+$('#client-edit-form').on('submit', (event) => {
   event.preventDefault();
 });
 
-if (("#client-edit-form").length) {
-  $("#client-edit-form").validate({
+if (('#client-edit-form').length) {
+  $('#client-edit-form').validate({
     rules: {
       name: {
         required: true,
@@ -247,48 +242,48 @@ if (("#client-edit-form").length) {
       },
       organization: {
         required: true,
-      }
+      },
     },
     messages: {
       name: {
-        required: "Name is required !!!",
+        required: 'Name is required !!!',
       },
       city: {
-        required: "City is required !!!",
+        required: 'City is required !!!',
       },
       state: {
-        required: "State is required !!!",
+        required: 'State is required !!!',
       },
       country: {
-        required: "Country is required !!!",
+        required: 'Country is required !!!',
       },
       organization: {
-        required: "Organization is required !!!",
-      }
+        required: 'Organization is required !!!',
+      },
     },
-    submitHandler: (form) => {
-      const clientData = $("#client-edit-form").serializeArray();
+    submitHandler: () => {
+      const clientData = $('#client-edit-form').serializeArray();
       const clientDataObj = {};
-      clientData.forEach(obj => {
+      clientData.forEach((obj) => {
         clientDataObj[obj.name] = obj.value;
       });
 
-      clientId = $("#client-edit-submit").val();
+      const clientId = $('#client-edit-submit').val();
       $.ajax({
         url: `/admin/clients/${clientId}`,
-        method: "PUT",
+        method: 'PUT',
         data: clientDataObj,
         success: () => {
-          alert("Client Data Updated Successfully...");
+          alert('Client Data Updated Successfully...');
           viewClientWithPagination();
         },
         error: (resData) => {
           alert(resData.responseJSON.errorMessage);
-        }
+        },
       });
-    }
+    },
   });
 }
 
-hideShowField(["#all-client", "#clients-add-div", "#clients-view-div"], ["#add-client", "#pagination", "#clients-data-body"]);
+hideShowField(['#all-client', '#clients-add-div', '#clients-view-div'], ['#add-client', '#pagination', '#clients-data-body']);
 fetchClientData(1);
--- src/seeders/20220401072526-demo-client.js
+++ src/seeders/20220401072526-demo-client.js
@@ -1,37 +1,42 @@
+/* eslint-disable strict */
+
 'use strict';
+
 const { v4: uuidv4 } = require('uuid');
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  // eslint-disable-next-line no-unused-vars
+  async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert(
       'Clients',
       [
         {
           id: uuidv4(),
-          name: "Client 1",
-          email: "client.1@bacancy.com",
-          slackId: "slackId1",
-          city: "Surat",
-          state: "Gujarat",
-          country: "India",
-          organization: "Bacancy Technology",
+          name: 'Client 1',
+          email: 'client.1@bacancy.com',
+          slackId: 'slackId1',
+          city: 'Surat',
+          state: 'Gujarat',
+          country: 'India',
+          organization: 'Bacancy Technology',
         },
         {
           id: uuidv4(),
-          name: "Client 2",
-          email: "client.2@bacancy.com",
-          slackId: "slackId2",
-          city: "Ahmedabad",
-          state: "Gujarat",
-          country: "India",
-          organization: "Simform Solutions",
-        }
+          name: 'Client 2',
+          email: 'client.2@bacancy.com',
+          slackId: 'slackId2',
+          city: 'Ahmedabad',
+          state: 'Gujarat',
+          country: 'India',
+          organization: 'Simform Solutions',
+        },
       ],
       {},
-    )
+    );
   },
 
-  async down (queryInterface, Sequelize) {
-      await queryInterface.bulkDelete('Clients', null, {})
-  }
+  // eslint-disable-next-line no-unused-vars
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Clients', null, {});
+  },
 };
