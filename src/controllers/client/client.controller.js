77f3ae3b3cc590f1815f11cf59838db0d35a7a0f
Manish Rathod <manish@manish.rathod>
2022-04-08 17:55:27 +0530

    fix: Eslint error resolved in client controller and validator

--- src/controllers/client/client.controller.js
+++ src/controllers/client/client.controller.js
@@ -1,12 +1,13 @@
 import { v4 as uuidv4 } from 'uuid';
+import { Op } from 'sequelize';
 import { successResponse, errorResponse } from '../../helpers/index';
 import { Client } from '../../models';
-import { Op } from 'sequelize';
 
 export const clientRegisterController = async (req, res) => {
   try {
-    const { name, email, slackId, city, state, country, organization } = req.body;
-    
+    const {
+      name, email, slackId, city, state, country, organization,
+    } = req.body;
     const clientDetails = {
       id: uuidv4(),
       name,
@@ -17,7 +18,7 @@ export const clientRegisterController = async (req, res) => {
       country,
       organization,
       isArchive: false,
-    }
+    };
     const isUserExist = await Client.findAll({ where: { [Op.or]: [{ email }, { slackId }] } });
     if (isUserExist.length) throw new Error('EmailID or slackID alreay exists in the database !!!');
 
@@ -26,64 +27,69 @@ export const clientRegisterController = async (req, res) => {
   } catch (error) {
     errorResponse(req, res, error.message, 409, error);
   }
-
-}
+};
 
 export const getAllClientController = async (req, res) => {
   try {
     const page = Number(req.query.page) || 1;
     const count = Number(req.query.count) || 6;
-    const sortBy= req.query.sortBy || 'name';
+    const sortBy = req.query.sortBy || 'name';
     const sortOrder = req.query.sortOrder || 'ASC';
-    const searchWord = req.query.searchWord || "";
-    
-    const allClients = await Client.findAll({ where: {isArchive : false, [Op.or]: [{name: {[Op.like]: `%${searchWord}%`}}, {email: {[Op.like]: `%${searchWord}%`}}, {slackId: {[Op.like]: `%${searchWord}%`}}, {city: {[Op.like]: `%${searchWord}%`}}, {state: {[Op.like]: `%${searchWord}%`}}, {country: {[Op.like]: `%${searchWord}%`}}, {organization: {[Op.like]: `%${searchWord}%`}}]}, attributes: ['id', 'name', 'email', 'slackId', 'organization'] , order: [[`${sortBy}`, `${sortOrder}`]], offset: (page-1)*count, limit: count });
+    const searchWord = req.query.searchWord || '';
+
+    const allClients = await Client.findAll({
+      where: { isArchive: false, [Op.or]: [{ name: { [Op.like]: `%${searchWord}%` } }, { email: { [Op.like]: `%${searchWord}%` } }, { slackId: { [Op.like]: `%${searchWord}%` } }, { city: { [Op.like]: `%${searchWord}%` } }, { state: { [Op.like]: `%${searchWord}%` } }, { country: { [Op.like]: `%${searchWord}%` } }, { organization: { [Op.like]: `%${searchWord}%` } }] }, attributes: ['id', 'name', 'email', 'slackId', 'organization'], order: [[`${sortBy}`, `${sortOrder}`]], offset: (page - 1) * count, limit: count,
+    });
     // if (!allClients.length) throw new Error('Employee data does not exist !!!');
-    const isDataBefore = await Client.findAll({ where: {isArchive : false, [Op.or]: [{name: {[Op.like]: `%${searchWord}%`}}, {email: {[Op.like]: `%${searchWord}%`}}, {slackId: {[Op.like]: `%${searchWord}%`}}, {city: {[Op.like]: `%${searchWord}%`}}, {state: {[Op.like]: `%${searchWord}%`}}, {country: {[Op.like]: `%${searchWord}%`}}, {organization: {[Op.like]: `%${searchWord}%`}}]}, order: [[`${sortBy}`, `${sortOrder}`]], limit: (page-1)*count });
-    const isDataAfter = await Client.findAll({ where: {isArchive : false, [Op.or]: [{name: {[Op.like]: `%${searchWord}%`}}, {email: {[Op.like]: `%${searchWord}%`}}, {slackId: {[Op.like]: `%${searchWord}%`}}, {city: {[Op.like]: `%${searchWord}%`}}, {state: {[Op.like]: `%${searchWord}%`}}, {country: {[Op.like]: `%${searchWord}%`}}, {organization: {[Op.like]: `%${searchWord}%`}}]}, order: [[`${sortBy}`, `${sortOrder}`]], offset: page*count });
-    
-    const paginationDetails = { before: isDataBefore.length , after: isDataAfter.length};
+    const isDataBefore = await Client.findAll({ where: { isArchive: false, [Op.or]: [{ name: { [Op.like]: `%${searchWord}%` } }, { email: { [Op.like]: `%${searchWord}%` } }, { slackId: { [Op.like]: `%${searchWord}%` } }, { city: { [Op.like]: `%${searchWord}%` } }, { state: { [Op.like]: `%${searchWord}%` } }, { country: { [Op.like]: `%${searchWord}%` } }, { organization: { [Op.like]: `%${searchWord}%` } }] }, order: [[`${sortBy}`, `${sortOrder}`]], limit: (page - 1) * count });
+    const isDataAfter = await Client.findAll({ where: { isArchive: false, [Op.or]: [{ name: { [Op.like]: `%${searchWord}%` } }, { email: { [Op.like]: `%${searchWord}%` } }, { slackId: { [Op.like]: `%${searchWord}%` } }, { city: { [Op.like]: `%${searchWord}%` } }, { state: { [Op.like]: `%${searchWord}%` } }, { country: { [Op.like]: `%${searchWord}%` } }, { organization: { [Op.like]: `%${searchWord}%` } }] }, order: [[`${sortBy}`, `${sortOrder}`]], offset: page * count });
+
+    const paginationDetails = { before: isDataBefore.length, after: isDataAfter.length };
     allClients.push(paginationDetails);
     successResponse(req, res, allClients, 200);
   } catch (error) {
     errorResponse(req, res, error.message, 500, error);
   }
-}
+};
 
 export const clientUpdateDataController = async (req, res) => {
   try {
-    const clientId = req.params.clientId;
-    const { name, city, state, country, organization } = req.body;
-    const updatedClient = await Client.update({ name, city, state, country, organization }, { returning: true, where: { id: clientId } });
+    const { clientId } = req.params;
+    const {
+      name, city, state, country, organization,
+    } = req.body;
+    const updatedClient = await Client.update({
+      name, city, state, country, organization,
+    }, { returning: true, where: { id: clientId } });
     successResponse(req, res, updatedClient[1], 200);
   } catch (error) {
     errorResponse(req, res, error.message, 500, error);
   }
-}
+};
 
 export const clientSoftDeleteController = async (req, res) => {
   try {
-    const clientId = req.params.clientId;
+    const { clientId } = req.params;
     const matchedClient = await Client.findOne({ where: { id: clientId } });
     if (!matchedClient) {
       errorResponse(req, res, 'Client does not exist !!!', 500, { error: `Client does not exist with id ${clientId} !!!` });
       return;
     }
-
+    // eslint-disable-next-line max-len
     const softDeletedCLient = await Client.update({ isArchive: true }, { returning: true, where: { id: clientId } });
     successResponse(req, res, softDeletedCLient[1], 200);
   } catch (error) {
     errorResponse(req, res, error.message, 500, error);
   }
-}
+};
 
 export const getOneClientController = async (req, res) => {
-  try{
-    const clientId = req.params.clientId;
-    const matchedClient = await Client.findOne({ where: {id: clientId} });
-    if(!matchedClient) throw new Error('Client data does not exist');
-    successResponse(req, res, matchedClient, 200);  
-  }catch (error) {
+  try {
+    const { clientId } = req.params;
+    const matchedClient = await Client.findOne({ where: { id: clientId } });
+    if (!matchedClient) throw new Error('Client data does not exist');
+    successResponse(req, res, matchedClient, 200);
+  } catch (error) {
     errorResponse(req, res, error.message, 500, error);
   }
-}
+};
--- src/controllers/client/client.validator.js
+++ src/controllers/client/client.validator.js
@@ -5,14 +5,15 @@ import { Client } from '../../models';
 const clientRegisterObject = joi.object({
   name: joi.string().trim(true).required(),
   email: joi.string().email().trim(true).required(),
-  slackId: joi.string().alphanum().min(3).trim(true).required(),
+  slackId: joi.string().alphanum().min(3).trim(true)
+    .required(),
   city: joi.string().trim(true).required(),
   state: joi.string().trim(true).required(),
   country: joi.string().trim(true).required(),
   organization: joi.string().trim(true).required(),
 });
 
-export const clientRegisterValidation = async(req, res, next) => {
+export const clientRegisterValidation = async (req, res, next) => {
   const payload = {
     name: req.body.name,
     email: req.body.email,
@@ -21,15 +22,15 @@ export const clientRegisterValidation = async(req, res, next) => {
     state: req.body.state,
     country: req.body.country,
     organization: req.body.organization,
-  }
+  };
 
   const { error } = clientRegisterObject.validate(payload);
-  if(error) {
+  if (error) {
     errorResponse(req, res, error.message, 206, error.details);
-  }else{
+  } else {
     next();
   }
-}
+};
 
 export const clientDataUpdateOject = joi.object({
   name: joi.string().trim(true).required(),
@@ -39,10 +40,10 @@ export const clientDataUpdateOject = joi.object({
   organization: joi.string().trim(true).required(),
 });
 
-export const clientUpdateDataValidation = async(req, res, next) => {
-  const clientId = req.params.clientId;
+export const clientUpdateDataValidation = async (req, res, next) => {
+  const { clientId } = req.params;
   const matchedClient = await Client.findOne({ where: { id: clientId } });
-  if(!matchedClient){
+  if (!matchedClient) {
     errorResponse(req, res, 'Client does not exist !!!', 500, { error: `Client does not exist with id ${clientId} !!!` });
     return;
   }
@@ -53,12 +54,12 @@ export const clientUpdateDataValidation = async(req, res, next) => {
     state: req.body.state,
     country: req.body.country,
     organization: req.body.organization,
-  }
+  };
 
   const { error } = clientDataUpdateOject.validate(payload);
-  if(error){
+  if (error) {
     errorResponse(req, res, error.message, 206, error.details);
-  }else{
+  } else {
     next();
   }
-}
+};
