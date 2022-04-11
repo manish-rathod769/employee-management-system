cbf44c25dcf3f44453c17e6d248fec2ed9992ac1
Manish Rathod <manish@manish.rathod>
2022-04-11 09:55:02 +0530

    style: code style change as per eslint standards for routes

--- src/routes/client.route.js
+++ src/routes/client.route.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import { clientRegisterValidation, clientUpdateDataValidation } from '../controllers/client/client.validator';
-import { clientRegisterController, getAllClientController, clientUpdateDataController, clientSoftDeleteController } from '../controllers/client/client.controller';
+import * as clietControllers from '../controllers/client/client.controller';
 
 const route = express.Router();
 
-route.get('/clients', getAllClientController);
-route.post('/clients', clientRegisterValidation, clientRegisterController);
-route.put('/clients/:clientId', clientUpdateDataValidation, clientUpdateDataController);
-route.delete('/clients/:clientId', clientSoftDeleteController);
+route.get('/clients', clietControllers.getAllClientController);
+route.post('/clients', clientRegisterValidation, clietControllers.clientRegisterController);
+route.put('/clients/:clientId', clientUpdateDataValidation, clietControllers.clientUpdateDataController);
+route.delete('/clients/:clientId', clietControllers.clientSoftDeleteController);
 
 module.exports = route;
