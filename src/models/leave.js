23887727d7daaaa996a9bbc187bbe5395596095e
apexa <apexa.patel@bacancy.com>
2022-04-12 11:10:07 +0530

    edit: model, migration and seeder file syntax changes

--- src/models/leave.js
+++ src/models/leave.js
@@ -1,8 +1,21 @@
-const {
-  Model,
-} = require('sequelize');
+// const {
+//   Model,
+// } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
+<<<<<<< HEAD
+=======
+  // class Leave extends Model {
+  //   /**
+  //    * Helper method for defining associations.
+  //    * This method is not a part of Sequelize lifecycle.
+  //    * The `models/index` file will call this method automatically.
+  //    */
+  //   // static associate(models) {
+  //   //   // define association here
+  //   // }
+  // }
+>>>>>>> edit: model, migration and seeder file syntax changes
   const Leave = sequelize.define('Leave', {
     id: {
       type: DataTypes.UUID,
@@ -34,10 +47,16 @@ module.exports = (sequelize, DataTypes) => {
       defaultValue: false,
     },
   });
+<<<<<<< HEAD
   Leave.associate = (models) => {
     Leave.belongsTo(models.Employee, {
       foreignKey: 'employeeId',
     })
   };
+=======
+  // Leave.associate = function (models) {
+
+  // };
+>>>>>>> edit: model, migration and seeder file syntax changes
   return Leave;
 };
