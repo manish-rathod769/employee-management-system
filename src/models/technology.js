27ee81532c59d53695b32dcaa5ad5629c97f76ef
krushit-dudhat <krushitdudhat2001@gmail.com>
2022-04-12 09:52:06 +0530

    fix: model syntax change init => difine

--- src/models/technology.js
+++ src/models/technology.js
@@ -1,30 +1,16 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Technology extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      //
-    }
-  }
-  Technology.init({
-    techName: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  const Technology = sequelize.define(
+    'Technology',
+    {
+      techName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      isArchive: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
     },
-    isArchive: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
-  }, {
-    sequelize,
-    modelName: 'Technology',
-  });
+  );
   return Technology;
 };
