0e562e8b46a1c7e0e52f7e37b9739690ea1ff540
Manish Rathod <manish@manish.rathod>
2022-04-12 09:40:37 +0530

    style: model, migration and sedder file code style changes as per boliler plate

--- src/migrations/20220401065622-create-client.js
+++ src/migrations/20220401065622-create-client.js
@@ -1,44 +1,36 @@
-/* eslint-disable strict */
-
-'use strict';
-
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Clients', {
-      id: {
-        type: Sequelize.UUID,
-        allowNull: false,
-        primaryKey: true,
-      },
-      name: {
-        type: Sequelize.STRING,
-      },
-      email: {
-        type: Sequelize.STRING,
-      },
-      slackId: {
-        type: Sequelize.STRING,
-      },
-      city: {
-        type: Sequelize.STRING,
-      },
-      state: {
-        type: Sequelize.STRING,
-      },
-      country: {
-        type: Sequelize.STRING,
-      },
-      organization: {
-        type: Sequelize.STRING,
-      },
-      isArchive: {
-        type: Sequelize.BOOLEAN,
-        defaultValue: false,
-      },
-    });
-  },
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Clients', {
+    id: {
+      type: Sequelize.UUID,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: {
+      type: Sequelize.STRING,
+    },
+    email: {
+      type: Sequelize.STRING,
+    },
+    slackId: {
+      type: Sequelize.STRING,
+    },
+    city: {
+      type: Sequelize.STRING,
+    },
+    state: {
+      type: Sequelize.STRING,
+    },
+    country: {
+      type: Sequelize.STRING,
+    },
+    organization: {
+      type: Sequelize.STRING,
+    },
+    isArchive: {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false,
+    },
+  }),
   // eslint-disable-next-line no-unused-vars
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Clients');
-  },
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('Clients'),
 };
