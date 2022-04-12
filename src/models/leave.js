e2f63bf563486c03958f65b5d5e7762172f6b3f0
apexa <apexa.patel@bacancy.com>
2022-04-12 11:22:40 +0530

    edit: delete button remove and toggle added in view leave

--- src/models/leave.js
+++ src/models/leave.js
@@ -3,19 +3,6 @@
 // } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
-<<<<<<< HEAD
-=======
-  // class Leave extends Model {
-  //   /**
-  //    * Helper method for defining associations.
-  //    * This method is not a part of Sequelize lifecycle.
-  //    * The `models/index` file will call this method automatically.
-  //    */
-  //   // static associate(models) {
-  //   //   // define association here
-  //   // }
-  // }
->>>>>>> edit: model, migration and seeder file syntax changes
   const Leave = sequelize.define('Leave', {
     id: {
       type: DataTypes.UUID,
@@ -47,16 +34,10 @@ module.exports = (sequelize, DataTypes) => {
       defaultValue: false,
     },
   });
-<<<<<<< HEAD
   Leave.associate = (models) => {
     Leave.belongsTo(models.Employee, {
       foreignKey: 'employeeId',
     })
   };
-=======
-  // Leave.associate = function (models) {
-
-  // };
->>>>>>> edit: model, migration and seeder file syntax changes
   return Leave;
 };
