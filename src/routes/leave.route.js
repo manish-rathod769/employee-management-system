e2d02945eee8e0a4fd19cb50f15afcf922d52f86
apexa <apexa.patel@bacancy.com>
2022-04-13 09:50:20 +0530

    edit: accept-reject route combined

--- src/routes/leave.route.js
+++ src/routes/leave.route.js
@@ -6,7 +6,6 @@ import * as leaveControlller from '../controllers/leave/leave.controller';
 
 const router = new express.Router();
 
-
 // developer
 router.get('/', leaveControlller.leaveForm);
 router.post('/add/leave', leaveValidator.leaveRegisterValidation, leaveControlller.addLeave);
@@ -14,9 +13,7 @@ router.get('/view/leave', leaveControlller.viewLeave);
 router.get('/view/leave/:id', leaveControlller.viewOneLeave);
 router.post('/update/leave/:id', leaveValidator.leaveUpdateValidation, leaveControlller.updateLeave);
 
-
 // pm
-router.get('/accept/leaves/:id', leaveControlller.acceptLeaves);
-router.get('/reject/leaves/:id', leaveControlller.rejectLeaves);
+router.put('/accept-reject/leave', leaveControlller.acceptRejectLeave);
 
 module.exports = router;
