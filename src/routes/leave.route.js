ac3916996ddd0acbd4a0eefddafe8088e3b1b95d
apexa <apexa.patel@bacancy.com>
2022-04-13 09:50:20 +0530

    edit: accept-reject route combined

--- src/routes/leave.route.js
+++ src/routes/leave.route.js
@@ -6,10 +6,15 @@ import * as leaveControlller from '../controllers/leave/leave.controller';
 import { verifyCookie } from '../middlewares/auth';
 import * as roleCheck from '../middlewares/role';
 
+<<<<<<< HEAD
 const router = express.Router();
+=======
+const router = new express.Router();
+>>>>>>> edit: accept-reject route combined
 
 // developer
 
+<<<<<<< HEAD
 router.get('/leave/add-view',verifyCookie, roleCheck.roleDEV(true) ,leaveControlller.leaveForm);
 router.post('/leave/add',verifyCookie, roleCheck.roleDEV(false) , leaveValidator.leaveRegisterValidation, leaveControlller.addLeave);
 router.get('/leave/view',verifyCookie, roleCheck.role_All(false) ,leaveControlller.viewLeave);
@@ -18,5 +23,9 @@ router.post('/leave/update/:id',verifyCookie, roleCheck.roleDEV(false) ,leaveVal
 
 // pm
 router.put('/leave/accept-reject',verifyCookie, roleCheck.rolePM(false), leaveControlller.acceptRejectLeave);
+=======
+// pm
+router.put('/accept-reject/leave', leaveControlller.acceptRejectLeave);
+>>>>>>> edit: accept-reject route combined
 
 module.exports = router;
