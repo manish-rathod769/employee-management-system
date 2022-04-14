64c89bd1648c9027825545eac50e25eb6b27295c
apexa <apexa.patel@bacancy.com>
2022-04-14 13:50:42 +0530

    add: pagination in all view leave page.

--- src/routes/leave.route.js
+++ src/routes/leave.route.js
@@ -10,6 +10,7 @@ const router = express.Router();
 
 // developer
 
+<<<<<<< HEAD
 router.get('/leave/add-view',verifyCookie, roleCheck.roleDEV(true) ,leaveControlller.leaveForm);
 router.post('/leave/add',verifyCookie, roleCheck.roleDEV(false) , leaveValidator.leaveRegisterValidation, leaveControlller.addLeave);
 router.get('/leave/view',verifyCookie, roleCheck.role_All(false) ,leaveControlller.viewLeave);
@@ -18,5 +19,15 @@ router.post('/leave/update/:id',verifyCookie, roleCheck.roleDEV(false) ,leaveVal
 
 // pm
 router.put('/leave/accept-reject',verifyCookie, roleCheck.rolePM(false), leaveControlller.acceptRejectLeave);
+=======
+router.get('/leave/add-view', leaveControlller.leaveForm);
+router.post('/leave/add', leaveValidator.leaveRegisterValidation, leaveControlller.addLeave);
+router.get('/leave/view', leaveControlller.viewLeave);
+router.get('/leave/view/:id', leaveControlller.viewOneLeave);
+router.post('/leave/update/:id', leaveValidator.leaveUpdateValidation, leaveControlller.updateLeave);
+
+// pm
+router.put('/leave/accept-reject', leaveControlller.acceptRejectLeave);
+>>>>>>> add: pagination in all view leave page.
 
 module.exports = router;
