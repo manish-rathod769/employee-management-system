28bf1d47d7619a8754ab0cb1e4efa97dcced12c9
apexa <apexa.patel@bacancy.com>
2022-04-14 13:50:42 +0530

    add: pagination in all view leave page.

--- src/controllers/leave/leave.controller.js
+++ src/controllers/leave/leave.controller.js
@@ -186,7 +186,6 @@ const updateLeave = async (req, res) => {
     const devId = req.user.id;
     let mailOptions = {};
     const getdata = await Leave.findByPk(req.params.id);
-    //console.log(getdata);
     const {
       employeeId, startDate, endDate, reason, status, isArchived,
     } = req.body;
