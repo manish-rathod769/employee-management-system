75a1c64c68b920ee7932fed1df82cd4d9b1ad7b5
Manish Rathod <manish@manish.rathod>
2022-04-25 14:15:28 +0530

    edit: Update and Add button removed for role except ADMIN

--- src/routes/client.route.js
+++ src/routes/client.route.js
@@ -6,7 +6,7 @@ import * as roleCheck from '../middlewares/role';
 
 const route = express.Router();
 
-route.get('/clients', verifyCookie, roleCheck.role_Admin_PM_HR(true), (req, res) => res.render('clients'));
+route.get('/clients', verifyCookie, roleCheck.role_Admin_PM_HR(true), (req, res) => res.render('clients', { role: req.user.role }));
 route.get('/getClients', checkAJAX, verifyCookie, roleCheck.role_Admin_PM_HR(false), clientControllers.getAllClient);
 route.get('/clients/:clientId', checkAJAX, verifyCookie, roleCheck.role_Admin_PM_HR(false), clientControllers.getOneClient);
 route.post('/clients', verifyCookie, roleCheck.roleAdmin(false), clientRegisterValidation, clientControllers.addNewClient);
