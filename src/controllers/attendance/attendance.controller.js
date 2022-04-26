d2cbcccaa53a233ad1c85224f8ab7e31abf48743
krushit-dudhat <krushitdudhat2001@gmail.com>
2022-04-26 19:40:41 +0530

    fix: refactor code

--- src/controllers/attendance/attendance.controller.js
+++ src/controllers/attendance/attendance.controller.js
@@ -1,7 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 import { Op } from 'sequelize';
 import * as helpers from '../../helpers';
-import { getTime, getTotalHour, getFinalLog } from '../../helpers/attendance';
+import { getTime, getTotalMinutes, getFinalLog } from '../../helpers/attendance';
 import { DailyAttendance } from '../../models';
 
 
@@ -21,13 +21,11 @@ export const addAttendance = async (req, res) => {
         log: [],
       },
     });
-    // console.log('INITIAL ATTENDANCE');
-    // console.log(JSON.stringify(attendance, null, 2));
 
     // check whether employee is on leave
     // if true then attendance.isOnLeave = true; totalHour = 0;
     if (attendance.isOnLeave) {
-      return helpers.errorResponse(req, res, 'employee is on leave');
+      return helpers.errorResponse(req, res, 'employee is on leave', 400);
     }
 
     // else if not on leave
@@ -36,11 +34,13 @@ export const addAttendance = async (req, res) => {
       if (getTime(new Date(req.body.startDate)) < getTime(new Date())) {
         return helpers.errorResponse(req, res, 'Invalid input date', 406);
       }
+
       // if last log only have start but not end key then new session can't start
       if (timeLog.toString() && !Object.prototype.hasOwnProperty.call(timeLog[(timeLog.length || 1) - 1], 'end')) {
         return helpers.errorResponse(req, res, 'one session is already running', 401);
       }
 
+      // take start date in boject and push it to array.
       const start = new Date(req.body.startDate);
       const obj = { start };
       timeLog.push(obj);
@@ -49,6 +49,7 @@ export const addAttendance = async (req, res) => {
       if (!timeLog) {
         return helpers.errorResponse(req, res, 'no session running', 401);
       }
+
       if (Object.prototype.hasOwnProperty.call(timeLog[timeLog.length - 1], 'end')) {
         return helpers.errorResponse(req, res, 'session is already ended', 401);
       }
@@ -58,7 +59,7 @@ export const addAttendance = async (req, res) => {
 
       // count total hour in current session and add to hour count
       // hour count stores total minutes.
-      attendance.totalMinutes = getTotalHour(
+      attendance.totalMinutes = getTotalMinutes(
         Number(attendance.totalMinutes),
         timeLog[timeLog.length - 1].start,
         timeLog[timeLog.length - 1].end,
@@ -84,12 +85,11 @@ export const addAttendance = async (req, res) => {
         plain: true,
       },
     );
-    // console.log('Daily TABLE |||| ----------||||| ----------------||||');
-    // console.log(JSON.stringify(result, null, 2));
+
     res.status(201);
     return helpers.successResponse(req, res, result, 201);
   } catch (error) {
-    return helpers.errorResponse(req, res, 'something went wrong', 500, error.message);
+    return helpers.errorResponse(req, res, 'Attendance punching Error', 500, error.message);
   }
 };
 
@@ -98,7 +98,7 @@ export const getAttendance = async (req, res) => {
     const date = new Date();
     const month = (Number(req.query.month) || date.getMonth()) + 1;
     const year = Number(req.query.year) || date.getFullYear();
-    // console.log(month, year);
+
     const attendance = await DailyAttendance.findAll(
       {
         attributes: ['date', 'finalLog', 'log', 'isOnLeave'],
@@ -109,19 +109,18 @@ export const getAttendance = async (req, res) => {
         },
       },
     );
-    // console.log(attendance);
+
     res.status(200);
     if (!attendance.toString()) {
-      helpers.successResponse(req, res, 'NA', 200);
-    } else {
-      helpers.successResponse(req, res, attendance, 200);
+      return helpers.successResponse(req, res, 'NA', 200);
     }
+    return helpers.successResponse(req, res, attendance, 200);
   } catch (error) {
-    helpers.errorResponse(req, res, 'something went wrong', 500, error.message);
+    return helpers.errorResponse(req, res, 'something went wrong', 500, error.message);
   }
 };
 
 export const getAttendanceView = (req, res) => {
   res.status(200);
-  res.render('employee/attendance');
+  return res.render('employee/attendance');
 };
